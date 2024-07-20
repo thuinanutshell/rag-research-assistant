90a26166562f4a5c49c407be0146f6e59654bd7a
Thu Than <121852798+thuinanutshell@users.noreply.github.com>
2024-07-20 23:09:01 +0900

    Update blogs.py

--- blogs.py
+++ blogs.py
@@ -2,20 +2,20 @@ from pathlib import Path
 
 import scrapy 
 
-# Define some attributes to the Spider
 class BlogPosts(scrapy.Spider):
     name = 'blogs'
-
-    def start_requests(self):
-        urls = [
-            "https://www.llamaindex.ai/blog/improving-vector-search-reranking-with-postgresml-and-llamaindex",
-            "https://www.llamaindex.ai/blog/introducing-llama-agents-a-powerful-framework-for-building-production-multi-agent-ai-systems"
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    
+    start_urls = [
+        "https://www.llamaindex.ai/blog"
+    ]
     
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f"blog_posts-{page}.pdf"
-        Path(filename).write_bytes(response.body)
-        self.log(f"Saved file {filename}")
+        for blog in response.css('div.CardBlog_card__mm0Zw p'):
+            title = blog.css('a::text').get()
+            link = blog.css('a::attr(href)').get()
+
+            if title is not None:
+                yield {
+                    'title': title,
+                    'link': link 
+                }
