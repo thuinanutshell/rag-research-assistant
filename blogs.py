9cdcc1e4945b7877ba806288863d422f7256e47e
Thu Than <121852798+thuinanutshell@users.noreply.github.com>
2024-07-21 17:48:13 +0900

    Add a function to scrape content from all href links

--- blogs.py
+++ blogs.py
@@ -2,20 +2,33 @@ from pathlib import Path
 
 import scrapy 
 
+# Define attributes to the Spider
 class BlogPosts(scrapy.Spider):
     name = 'blogs'
-    
-    start_urls = [
-        "https://www.llamaindex.ai/blog"
-    ]
-    
+    start_urls = ["https://www.llamaindex.ai/blog"]
+
     def parse(self, response):
+        blog_lists = []
+        # Loop through all the blog cards on the page with the defined class
+        # Extract the title and the href link using the get() method
         for blog in response.css('div.CardBlog_card__mm0Zw p'):
             title = blog.css('a::text').get()
             link = blog.css('a::attr(href)').get()
 
+           # Return the list consisting of title and link
             if title is not None:
-                yield {
+                blog_lists.append({
                     'title': title,
-                    'link': link 
-                }
+                    'link': response.urljoin(link) 
+               })
+        yield {'blogs': blog_lists}
+
+        for blog in blog_lists:
+            yield scrapy.Request(blog['link'], callback=self.parse_blog_content)
+     
+    def parse_blog_content(self, response):
+        content = response.css("div.BlogPost_htmlPost__Z5oDL p::text").getall()
+        yield {
+            'url': response.url,
+            'content': content
+        }
