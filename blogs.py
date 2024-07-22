3501ea2f28785a44f498e0609236e62b2001262a
Thu Than <121852798+thuinanutshell@users.noreply.github.com>
2024-07-22 15:53:39 +0900

    Update blogs.py
    include code, figures and paragraphs in the content key

--- blogs.py
+++ blogs.py
@@ -9,29 +9,26 @@ class BlogPosts(scrapy.Spider):
 
     def parse(self, response):
         blog_lists = []
-        # Loop through all the blog cards on the page with the defined class
+        # Loop through all the blog cards on the page with the class name
         # Extract the title and the href link using the get() method
         for blog in response.css('div.CardBlog_card__mm0Zw p'):
             title = blog.css('a::text').get()
             link = blog.css('a::attr(href)').get()
 
-           # Add items to the blog_lists list
+           # Return the list consisting of title and link
             if title is not None:
                 blog_lists.append({
                     'title': title,
-                    'link': response.urljoin(link) # convert to absolute urls
+                    'link': response.urljoin(link) 
                })
-        # Return a dictionary with the key 'blogs' and its value
-        yield {'blogs': blog_lists}
 
-        # Loop through each href link in the blog_lists and request from each blog link
         for blog in blog_lists:
             yield scrapy.Request(blog['link'], callback=self.parse_blog_content)
-
-    # Create another function to get the content from each url 
+     
     def parse_blog_content(self, response):
-        content = response.css("div.BlogPost_htmlPost__Z5oDL p::text").getall()
+        content = response.css("div.BlogPost_htmlPost__Z5oDL").getall()
         yield {
             'url': response.url,
-            'content': content
+            'title': response.css("h1::text").get(),
+            'content': content,
         }
