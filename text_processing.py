3068ffbd9d680593fbe93bfcdd8c6daf1092f0d1
Thu Than <121852798+thuinanutshell@users.noreply.github.com>
2024-07-22 15:51:38 +0900

    Update preprocessing text
    To preserve the context of the blog content, I decided to not separate the code and figure separately.

--- text_processing.py
+++ text_processing.py
@@ -1,5 +1,5 @@
 import re
-import unicodedata
+from cleantext import clean
 import json
 import nltk
 from nltk.corpus import stopwords
@@ -7,42 +7,44 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 nltk.download('stopwords')
 
-with open('input/blogs.json') as file:
+with open('input/content.json') as file:
     data = json.load(file)
 
+# Text Preprocessing Before Chunking
 """
-- lowercase
+Preprocess the text data by:
+- remove html tags
+- transform to lowercase
 - remove stopwords
-- remove punctuations
+- remove whitespae
 - remove special characters such as emojis
 """
-stop_words = set(stopwords.words("english"))
-pattern = r'[^a-zA-Z0-9\s]'
-lowercase_data = [
-   {'url': item['url'],
-     'title': word_tokenize(re.sub(pattern, '', item['title']).lower()),
-     'content': word_tokenize(re.sub(pattern, '', ''.join(item['content'])).lower())}
-    for item in data
-]
 
-def remove_stopwords(data):
-    cleaned_data = []
-    for item in data:
-       cleaned_item = {
-           'url': item['url'],
-           'title': [word for word in item['title'] if word not in stop_words],
-           'content': [word for word in item['content'] if word not in stop_words]
-       }
-       cleaned_data.append(cleaned_item)
-    return cleaned_data
+# Define patterns to remove from the text
+html_tags = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
+whitespace = re.compile(r'\s+')
+stopwords = set(stopwords.words('english'))
 
-cleaned_data = remove_stopwords(lowercase_data)
+
+def preprocess_text(text):
+    for item in text:
+        item['content'] = ' '.join(item['content'])
+        item['content'] = re.sub(html_tags, '', item['content'])
+        item['content'] = clean(item['content'], no_emoji=True)
+        item['content'] = re.sub(whitespace,' ', item['content']).strip()
+        item['content'] = item['content'].lower()
+    return text
+
+def remove_stopwords(text):
+    for item in text:
+        item['content'] = ' '.join([word for word in word_tokenize(item['content']) if word not in stopwords])
+    return text
+
+cleaned_data = preprocess_text(data)
+cleaned_data = remove_stopwords(cleaned_data)
 
 # Export the cleaned data into a JSON file
 output_file = 'cleaned_text.json'
 
 with open(output_file, 'w', encoding='utf-8') as file:
     json.dump(cleaned_data, file, ensure_ascii=False, indent=4)
-
-print(f"Data has been cleaned and saved to {output_file}")
-
